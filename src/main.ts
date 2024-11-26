f492f191e7ca639740e6e6affbdfcf57d7438023
Jatingupta9120 <jatinofficial9120@gmail.com>
2024-11-26 23:57:23 +0530

    feat: added some modificatiion

--- src/main.ts
+++ src/main.ts
@@ -79,10 +79,14 @@ async function bootstrap() {
         }
 
         app.enableCors({
-            origin: ['http://localhost:5174', 'http://localhost:5173'], // Allow frontend running on this port
-            methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allow specific HTTP methods
-            allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-            credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+            origin: [
+                'http://localhost:5174',
+                'http://localhost:5173',
+                'http://localhost:8080', // No trailing slash
+            ],
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+            credentials: true,
         });
 
         const config = new DocumentBuilder()
